a9fcbf6ac87f0afa30bb5400a8a7f10e2573348c
aidanjrauscher <ajrscomputer@gmail.com>
2023-08-19 20:55:31 -0400

    testing

---
+++ test.js
@@ -0,0 +1,3 @@
+import testModulo from "./tests/modulo.js";
+
+testModulo()
--- tests/modulo.js
+++ tests/modulo.js
@@ -1,23 +1,25 @@
-import { modulo } from "../math.js";
+import { modulo } from "../math.js"
 
-const calculatedResult1 = modulo(17,3)
-const expectedResult1 = 2
-equalityCheck(1, calculatedResult1, expectedResult1)
+export default function testModulo(){
+    const calculatedResult1 = modulo(17,3)
+    const expectedResult1 = 2
+    equalityCheck(1, calculatedResult1, expectedResult1)
 
-const calculatedResult2 = modulo(49,7)
-const expectedResult2 = 0
-equalityCheck(2, calculatedResult2, expectedResult2)
+    const calculatedResult2 = modulo(49,7)
+    const expectedResult2 = 0
+    equalityCheck(2, calculatedResult2, expectedResult2)
 
-const calculatedResult3 = modulo(13,0)
-const expectedResult3 = 0
-equalityCheck(3, calculatedResult3, expectedResult3)
+    const calculatedResult3 = modulo(13,0)
+    const expectedResult3 = 0
+    equalityCheck(3, calculatedResult3, expectedResult3)
 
 
-function equalityCheck(testNum, val1, val2){
-    if(val1 === val2){
-        console.log(`PASSED: TEST ${testNum}`)
-    }
-    else{
-        console.log(`FAILED: TEST ${testNum}`)
+    function equalityCheck(testNum, val1, val2){
+        if(val1 === val2){
+            console.log(`PASSED: TEST ${testNum}`)
+        }
+        else{
+            console.log(`FAILED: TEST ${testNum}`)
+        }
     }
 }
